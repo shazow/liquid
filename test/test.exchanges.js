d8a8c78e199ed59d98a6587a3036d6ef4e04a887
Andrey Petrov <andrey.petrov@shazow.net>
2014-07-17 15:06:31 -0700

    More dummy stubs and tests.

--- test/test.exchanges.js
+++ test/test.exchanges.js
@@ -13,9 +13,23 @@ describe('Dummy Exchange', function() {
         });
     });
 
-    it('should start empty', function() {
-        var orders = exchange.getOrders();
-        assert.deepEqual(orders, {
+    it('should replace all orders', function() {
+        var startOrders = {
+            'bids': ['foo'],
+            'asks': ['bar', 'baz']
+        }
+        exchange.replaceOrders(startOrders);
+        assert.deepEqual(exchange.getOrders(), startOrders);
+
+        var newOrders = {
+            'bids': ['baz', 'etc'],
+            'asks': ['quux']
+        }
+        exchange.replaceOrders(newOrders);
+        assert.deepEqual(exchange.getOrders(), newOrders);
+
+        exchange.replaceOrders({});
+        assert.deepEqual(exchange.getOrders(), {
             'bids': [],
             'asks': []
         });
