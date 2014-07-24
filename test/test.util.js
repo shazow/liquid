01939935b3997de51ffe6993474ac1282625b6b0
Andrey Petrov <andrey.petrov@shazow.net>
2014-07-23 22:27:33 -0700

    Exchanges and bot has functional event loops and clean shutdown.

---
+++ test/test.util.js
@@ -0,0 +1,47 @@
+var assert = require('assert'),
+    util = require('../lib/util.js');
+
+
+describe('util.hasStringValues', function() {
+    it('should succeed on all present values', function() {
+        var r = util.hasStringValues({
+            'foo': 'a',
+            'bar': 'bb',
+            'baz': 'ccc'
+        }, ['foo', 'bar', 'baz']);
+        assert(r);
+
+        var r = util.hasStringValues({
+            'foo': 'a',
+        }, ['foo']);
+        assert(r);
+
+        var r = util.hasStringValues({}, []);
+        assert(r);
+    });
+
+    it('should fail on missing value', function() {
+        var r = util.hasStringValues({
+            'foo': 'a',
+            'bar': 'bb',
+        }, ['foo', 'bar', 'baz']);
+        assert.equal(r, false);
+    });
+
+    it('should fail on falsey values', function() {
+        var r = util.hasStringValues({
+            'foo': '',
+        }, ['foo']);
+        assert.equal(r, false);
+
+        var r = util.hasStringValues({
+            'foo': false,
+        }, ['foo']);
+        assert.equal(r, false);
+
+        var r = util.hasStringValues({
+            'foo': 0,
+        }, ['foo']);
+        assert.equal(r, false);
+    });
+});
