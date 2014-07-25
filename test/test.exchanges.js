cbe395d593d9e13f706c9d7c5ef8c1e576ee22d1
Andrey Petrov <andrey.petrov@shazow.net>
2014-07-24 21:35:00 -0700

    Progress: Migrate more code to new state format. 1 failing test.

--- test/test.exchanges.js
+++ test/test.exchanges.js
@@ -16,10 +16,7 @@ describe('Exchanges', function() {
         it('should start empty', function() {
             var exchange = new DummyExchange();
             var orders = exchange.getOrders();
-            assert.deepEqual(orders, {
-                'bids': [],
-                'asks': []
-            });
+            assert.deepEqual(orders, []);
         });
 
         it('should increment the internal counter', function() {
@@ -31,9 +28,7 @@ describe('Exchanges', function() {
             var o2 = new Order('hascount', 'BID', 1, 1);
             var o3 = new Order(null, 'BID', 1, 1);
 
-            exchange.placeOrders({
-                'asks': [o1, o2, o3],
-            });
+            exchange.placeOrders([o1, o2, o3]);
 
             assert.equal(exchange.idCounter, 2);
             assert.equal(o1.id, '1');
@@ -44,25 +39,24 @@ describe('Exchanges', function() {
         it('should replace all orders', function() {
             var exchange = new DummyExchange();
 
-            var startOrders = {
-                'bids': [new Order('foo', 'BID', 1, 1)],
-                'asks': [new Order('bar', 'BID', 1, 1), new Order('baz', 'BID', 1, 1)]
-            };
+            var startOrders = [
+                new Order('foo', 'BID', 1, 1),
+                new Order('bar', 'ASK', 1, 1),
+                new Order('baz', 'ASK', 1, 1)
+            ];
             exchange.replaceOrders(startOrders);
             assert.deepEqual(exchange.getOrders(), startOrders);
 
-            var newOrders = {
-                'bids': [new Order('baz', 'BID', 1, 1), new Order('etc', 'BID', 1, 1)],
-                'asks': [new Order('quux', 'BID', 1, 1)]
-            };
+            var newOrders = [
+                new Order('baz', 'BID', 1, 1),
+                new Order('etc', 'BID', 1, 1),
+                new Order('quux', 'ASK', 1, 1)
+            ];
             exchange.replaceOrders(newOrders);
             assert.deepEqual(exchange.getOrders(), newOrders);
 
-            exchange.replaceOrders({});
-            assert.deepEqual(exchange.getOrders(), {
-                'bids': [],
-                'asks': []
-            });
+            exchange.replaceOrders([]);
+            assert.deepEqual(exchange.getOrders(), []);
         });
     });
 
