61567d261792cb21a5d8e17300c2ecde4efa2492
Andrey Petrov <andrey.petrov@shazow.net>
2014-08-03 13:20:17 -0700

    Util updates: mergeArrays, mergeObjects, missingStringValues

--- test/test.util.js
+++ test/test.util.js
@@ -3,47 +3,61 @@ var assert = require('assert'),
 
 
 describe('Util', function() {
-    describe('hasStringValues', function() {
+    describe('missingStringValues', function() {
         it('should succeed on all present values', function() {
-            var r = util.hasStringValues({
+            var r = util.missingStringValues({
                 'foo': 'a',
                 'bar': 'bb',
                 'baz': 'ccc'
             }, ['foo', 'bar', 'baz']);
-            assert(r);
+            assert.equal(r.length, 0);
 
-            var r = util.hasStringValues({
+            var r = util.missingStringValues({
                 'foo': 'a',
             }, ['foo']);
-            assert(r);
+            assert.equal(r.length, 0);
 
-            var r = util.hasStringValues({}, []);
-            assert(r);
+            var r = util.missingStringValues({}, []);
+            assert.equal(r.length, 0);
         });
 
         it('should fail on missing value', function() {
-            var r = util.hasStringValues({
+            var r = util.missingStringValues({
                 'foo': 'a',
                 'bar': 'bb',
             }, ['foo', 'bar', 'baz']);
-            assert.equal(r, false);
+            assert.deepEqual(r, ['baz']);
         });
 
         it('should fail on falsey values', function() {
-            var r = util.hasStringValues({
+            var r = util.missingStringValues({
                 'foo': '',
             }, ['foo']);
-            assert.equal(r, false);
+            assert.deepEqual(r, ['foo']);
 
-            var r = util.hasStringValues({
+            var r = util.missingStringValues({
                 'foo': false,
             }, ['foo']);
-            assert.equal(r, false);
+            assert.deepEqual(r, ['foo']);
 
-            var r = util.hasStringValues({
+            var r = util.missingStringValues({
                 'foo': 0,
             }, ['foo']);
-            assert.equal(r, false);
+            assert.deepEqual(r, ['foo']);
+        });
+    });
+
+    describe('mergeArrays', function() {
+        it('should merge arrays', function() {
+            var r = util.mergeArrays([], [1,2,3], [], [3,4,5,6], []);
+            assert.deepEqual(r, [1,2,3,3,4,5,6]);
+        });
+    });
+
+    describe('mergeObjects', function() {
+        it('should merge objects', function() {
+            var r = util.mergeObjects({}, {'foo': 123, 'bar': 234}, {}, {'bar': 345, 'baz': 456}, {});
+            assert.deepEqual(r, {'foo': 123, 'bar': 345, 'baz': 456});
         });
     });
 });
